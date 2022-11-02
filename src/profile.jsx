191e03e3edeab4ec500e34f4040fe128a22046ac
nicogarciaa <115661364+nicogarciaa@users.noreply.github.com>
2022-11-02 16:22:14 -0300

    profile

--- src/profile.jsx
+++ src/profile.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import "./profile.css"
 import arrowLeft from "../src/components/data/img/arrow-left.svg"
 import pokebola from "../src/components/data/img/Pokeball.png"
+import bulbasaur from "../src/components/data/img/bulbasaur.png"
+import frame from "../src/components/data/img/Frame.svg"
 
 
 const profile = () => {
@@ -10,15 +12,21 @@ const profile = () => {
 
     <header>
         <div className="header1">
-    <img className='arrowLeft' src={arrowLeft} alt="" />
+    <img className='arrowLeft' src={arrowLeft} alt="Flecha" />
     <h1 className='h1Header'>Bulbasaur</h1>
     <h2 className='h2Header'>#001</h2>
     </div>
-    <img className='pokebola' src={pokebola} alt="" />
+    <img className='frame' src={frame} alt="Frame" />
+    <img className='pokebola' src={pokebola} alt="Pokebola" />
     </header>
 
     <main>
-
+    <img className='bulbasaur' src={bulbasaur} alt="Bulbasaur" />
+    <div className="buttones">
+        <button className='verde'>Grass</button>
+        <button className='violeta'>Polson</button>
+    </div>
+    <p>About</p>
     </main>
     </div>
   )
