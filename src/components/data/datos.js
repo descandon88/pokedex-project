6e862a6618805b36bd9a4385d52659f507d28e82
David <david.escandon.v@gmail.com>
2022-11-02 09:51:52 -0300

    api pokemon

---
+++ src/components/data/datos.js
@@ -0,0 +1,42 @@
+let pokemons = [
+  {
+    nombre: "Bulbasour",
+    id: "001",
+    img: "./img/bulbasaur.png",
+  },
+  {
+    nombre: "Charmander",
+    id: "004",
+    img: "./img/charmander.png",
+  },
+  {
+    nombre: "Squirtle",
+    id: "007",
+    img: "./img/squirtle.png",
+  },
+  {
+    nombre: "Butterfree",
+    id: "012",
+  },
+  {
+    nombre: "Picachu",
+    id: "025",
+  },
+  {
+    nombre: "Gostly",
+    id: "092",
+  },
+  {
+    nombre: "Ditto",
+    id: "132",
+  },
+  {
+    nombre: "Mew",
+    id: "162",
+    img: "./img/mew.png",
+  },
+  {
+    nombre: "Aron",
+    id: "304",
+  },
+];
